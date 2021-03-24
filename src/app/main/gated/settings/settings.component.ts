64ee9ac26a865b710058513bc8cece8de8cf23e7
Gabe <djgtraxx@gmail.com>
2021-03-24 21:08:53 +0000

    successfully generating 32 bit key from master string, converting the key to array of ascii codes values, then to string, then back all the way to array buffer.  this proves it can be saved in localstorage, but first i still am going to cipher the string before it gets stored in localstorage

--- src/app/main/gated/settings/settings.component.ts
+++ src/app/main/gated/settings/settings.component.ts
@@ -20,11 +20,38 @@ export class SettingsComponent implements OnInit {
   async btnClick() {
     const theKeyBuff = await this.deriveKey(this.currentPW)
     const theKey = this.ab2str(theKeyBuff);
-    console.log(`the key: ${theKey}`);
+    const ciphertextValue = `${theKey}...[${theKeyBuff.byteLength} bytes total]`;
+    console.log(`the key: ${ciphertextValue}`);
+    console.log(String.fromCharCode.apply(null, new Uint8Array(theKeyBuff)));
+
+    // now for testing, we will turn it back into an array buffer, then back to a string again, to see if it stays the same...
+    const back2AB = this.str2ab(theKey);
+    const theKeyAgain = this.ab2str(back2AB);
+    const ciphertextValueAgain = `${theKeyAgain}...[${back2AB.byteLength} bytes total]`;
+    console.log(`the key: again ${ciphertextValueAgain}`);
+    console.log(String.fromCharCode.apply(null, new Uint8Array(back2AB)));
   }
 
-  ab2str(buf) {
-    return String.fromCharCode.apply(null, new Uint8Array(buf));
+  ab2str(buf): string {
+    const asString =  String.fromCharCode.apply(null, new Uint8Array(buf));
+    //const buffer = new Uint8Array(buf, 0, 5).toString();
+    //return buffer;
+
+    const arrStringCodes = Array();
+    for (let i = 0; i < asString.length; i++) {
+      const currentCode = asString.charCodeAt(i);
+      arrStringCodes[i] = currentCode;
+      //console.log(currentCode);
+    }
+
+    return arrStringCodes.toString();
+  }
+
+  str2ab(str: string) {
+    let strArray = Array();
+    strArray = str.split(',');
+    const data = Uint8Array.from(strArray);
+    return data.buffer;
   }
 
   async deriveKey(password) {
