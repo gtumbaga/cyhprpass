8827cef59c9b82153cf35ef0fd5c081a5f52cc83
Gabe <djgtraxx@gmail.com>
2021-03-25 09:55:28 +0000

    got some of the styling and layout done, and added a shared service to track toggles, and other global data

--- src/app/main/gated/settings/settings.component.ts
+++ src/app/main/gated/settings/settings.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
 import { CryptoService } from '../../services/crypto/crypto.service';
+import { SharedService } from '../../services/shared/shared.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent implements OnInit {
-  currentPW: string;
+export class SettingsComponent implements OnInit, AfterContentChecked {
+  userPW: string;
+  userPIN: string;
 
-  constructor(private cryptoService: CryptoService) {
+  constructor(private cryptoService: CryptoService, public sharedService: SharedService) {
     //
   }
 
   ngOnInit(): void {
   }
 
+  ngAfterContentChecked(): void {
+    this.sharedService.toggleHeader(true);
+  }
+
   async btnClick(): Promise<void> {
-    const theKeyBuff = await this.cryptoService.deriveKeyFromMasterString(this.currentPW);
+    const theKeyBuff = await this.cryptoService.deriveKeyFromMasterString(this.userPW);
     const theKey = this.cryptoService.ab2str(theKeyBuff);
     const ciphertextValue = `${theKey}...[${theKeyBuff.byteLength} bytes total]`;
     console.log(`the key: ${ciphertextValue}`);
